315ffff5a96dea2bd9deba3f43e2ef5bf627aa7e
Samuel Ruiz <28467342+SamPuckett@users.noreply.github.com>
2020-11-11 22:49:09 -0800

    Update Button.jsx

--- src/Components/Body/Button.jsx
+++ src/Components/Body/Button.jsx
@@ -4,7 +4,7 @@ import "./Button.css";
 const Button = ({ buttonName }) => {
   return (
     <div className='button'>
-      <a href='mailto:samuelruiz1994@gmail.com'>{buttonName}</a>
+      <a href='mailto:samuelruizdev@gmail.com'>{buttonName}</a>
     </div>
   );
 };
