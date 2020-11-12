3ca77837f867a8e0042b9ffad9e8084c5ec44859
Samuel Ruiz <28467342+SamPuckett@users.noreply.github.com>
2020-11-11 22:49:58 -0800

    Update ResumeButton.jsx

--- src/Components/Nav/ResumeButton.jsx
+++ src/Components/Nav/ResumeButton.jsx
@@ -4,7 +4,7 @@ import "./ResumeButton.css";
 const ResumeButton = () => {
   return (
     <div className='resume-button'>
-      <a href='samuel-ruiz-resume.pdf'>Resume</a>
+      <a href='SamuelResume.pdf'>Resume</a>
     </div>
   );
 };
